221874838f9760578aeb87f88d01650abd21b39b
sandrasigismondi <sandra.sigismondi22@gmail.com>
2026-04-30 10:55:34 +0200

    Create numeri.R

---
+++ R/numeri.R
@@ -0,0 +1,12 @@
+# my function
+
+numeri <- function(x){
+  if(x>0){
+    print("Questo numero è positivo")
+   }
+  else if(x<0) {
+    print("Questo numero è negativo")
+   }
+    else {"Zero"
+    }
+  }
